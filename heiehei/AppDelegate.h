a909686f12dc38faef5cfac31284426e8c5cb5d4
my-secret <liuwei@liuweidemacbook-air.local>
2016-02-19 15:51:57 +0800

    Initial Commit

---
+++ heiehei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  heiehei
+//
+//  Created by 刘炜 on 16/2/19.
+//  Copyright © 2016年 刘炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ heiehei/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  heiehei
+//
+//  Created by 刘炜 on 16/2/19.
+//  Copyright © 2016年 刘炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
